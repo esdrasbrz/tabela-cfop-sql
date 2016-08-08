e1e230ce18a7bc73496c870ed1f98be6232f0fa1
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-08-08 00:40:02 -0300

    Primeiro commit

---
+++ cria_script_cfop.py
@@ -0,0 +1,24 @@
+"""
+Script que lê o CSV com todos os cfop e gera um sql para inserir no BD
+"""
+
+# abre o arquivo csv
+with open('tabela-cfop', 'r') as cfop_file:
+    # esqueleto do sql
+    sql = "INSERT INTO controle_estoque_cfop (cfop, descricao) values "
+
+    # lê as linhas
+    for linha in cfop_file.readlines():
+        cfop, descricao = linha.split(';')
+        descricao = descricao.replace('"', '', 2)
+        descricao = descricao.replace('\n', '')
+
+        # adiciona no sql
+        sql += "('%s', '%s'), " % (cfop, descricao.upper())
+
+# retira a vírgula do final
+sql = sql[0:-2] + ';'
+
+# salva o sql em um arquivo separado
+with open('cfop.sql', 'w') as sql_file:
+    sql_file.write(sql)
